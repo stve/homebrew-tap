ea2b0837f7a5c1d22fed53c4cb58e0c7dfdbd7ac
Steve Agalloco <steve.agalloco@gmail.com>
2017-11-27 15:26:46 -0500

    Update to scar 1.1.1

--- Formula/scar.rb
+++ Formula/scar.rb
@@ -1,9 +1,9 @@
 class Scar < Formula
   desc "Soundcloud artwork downloader"
   homepage "https://github.com/stve/scar"
-  url "https://github.com/stve/scar/releases/download/v1.1.0/scar_Darwin_x86_64.tar.gz"
-  version "v1.1.0"
-  sha256 "cda9236f11c61c473c45ebdd5b946430befc8cd3bf7a88b8b81781be4f63167c"
+  url "https://github.com/stve/scar/releases/download/v.1.1.1/scar_Darwin_x86_64.tar.gz"
+  version "v1.1.1"
+  sha256 "82eeacd4fe2df80ab518c09ae6c833127aaa0eeaf83cf2d0c7f4a0055a20e679"
 
   def install
     bin.install "scar"
